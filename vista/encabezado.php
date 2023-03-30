c3dfe7008e5f8b33595d80c35c6131ecf033d881
CaterinMB <127351464+CaterinMB@users.noreply.github.com>
2023-03-30 12:59:26 -0500

    Add files via upload

---
+++ vista/encabezado.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estiloEncabezado.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DynaPuff&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <h1>Pach_OS</h1>
+
+        <a href="../informe/informeGeneral.php">INFORME</a>
+        <a href="../Ventas/visualizarVentas.php">VENTAS</a>
+        <a href="../Productos/Productos.php">PRODUCTOS</a>
+        <a href="../Insumos/isumos.php">INSUMOS</a>
+        <a href="../Compras/Compras.php">COMPRAS</a>
+        <a href="../Usuarios/visualizarUsuario.php">USUARIOS</a>
+        <a href="../Proveedores/provedores.php">PROVEEDORES</a>
+
+        <button class="bton-cerrarSesion">CONFIGURACIÓN</button>
+    </nav>
+
+    
+    <section>
+        <div class="encabezado">
+            <p class="modulo">INFORME GENERAL</p>
+            <p class="perfil">Jeison Jimenez</p>
+        </div>
+        
+        
+
+        
+
+        
+    
