ec8bcec428451e07afeebf106c313d3eae2de737
CaterinMB <127351464+CaterinMB@users.noreply.github.com>
2023-03-30 13:07:33 -0500

    Add files via upload

---
+++ vista/Proveedores/inProvedores.php
@@ -0,0 +1,82 @@
+    <?php
+        include '../encabezado.php';
+    ?>
+
+    
+        <a href="provedores.php" >volver</a>
+        
+        
+        <table>
+            <tr>
+                <th class="id-usuarios">ID</th>
+                <th class="nombre-usuarios">Nombre</th>
+                <th class="rol-usuarios">Cantidad</th>
+                
+            </tr>
+            <tr>
+                <td>01</td> 
+                <td>Leche</td>
+                <td>6L</td>
+                
+            </tr>
+            <tr>
+                <td>02</td> 
+                <td>Harina</td>
+                <td>10Kg</td>
+                
+            </tr>
+            <tr>
+                <td>03</td> 
+                <td>Huevo</td>
+                <td>30</td>
+                
+            </tr>
+            <tr>
+                <td>04</td> 
+                <td>Pechuga de Pollo</td>
+                <td>15Kg</td>
+                
+            </tr>
+            <tr>
+                <td>05</td> 
+                <td>Mora</td>
+                <td>5k</td>
+                
+            </tr>
+            <tr>
+                <td>06</td> 
+                <td>Jamon</td>
+                <td>4LB</td>
+                
+            </tr>
+            <tr>
+                <td>07</td> 
+                <td>Piña</td>
+                <td>3K</td>
+                
+            </tr>
+            <tr>
+                <td>08</td> 
+                <td>Crema de leche</td>
+                <td>2L</td>
+                
+            </tr>
+            <tr>
+                <td>09</td> 
+                <td>Levadura</td>
+                <td>6k</td>
+                
+            </tr>
+            <tr>
+                <td>10</td> 
+                <td>Cocaina</td>
+                <td>Mucha</td>
+                
+            </tr>
+        </table>
+</div>
+    </div>
+
+    <?php
+        include '../PiePagina.php';
+    ?>
---
+++ vista/Proveedores/provedores.php
@@ -0,0 +1,233 @@
+<?php
+    include '../encabezado.php';
+?>
+        <link rel="stylesheet" href="../css/estiloProveedores.css">
+
+        <button onclick="abrirVentanaPR()" class="bton-registrar">Registrar Proveedor</button>
+        
+        <article class="contenido-Modulos">
+
+        
+        <table>
+
+            <tr>
+                <th class="id-usuarios">ID</th>
+                <th class="nombre-usuarios">Nombre Local</th>
+                <th class="rol-usuarios">Dirección</th>
+                <th class="correo-usuarios">Correo</th>
+                <th class="celular-usuarios">Celular</th>
+                <th class="celular-accion">Acción</th>
+            </tr>
+            <tr>
+                <td>01</td> 
+                <td>NA</td>
+                <td>Minorista</td>
+                <td>Na@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>02</td> 
+                <td>NE</td>
+                <td>Minorista</td>
+                <td>NE@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>03</td> 
+                <td>NI</td>
+                <td>Minorista</td>
+                <td>NI@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>04</td> 
+                <td>NO</td>
+                <td>Minorista</td>
+                <td>NO@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>05</td> 
+                <td>NU</td>
+                <td>Minorista</td>
+                <td>NU@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>06</td> 
+                <td>SA</td>
+                <td>Minorista</td>
+                <td>SA@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>07</td> 
+                <td>SE</td>
+                <td>Minorista</td>
+                <td>SE@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>08</td> 
+                <td>SI</td>
+                <td>Minorista</td>
+                <td>SI@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>09</td> 
+                <td>SO</td>
+                <td>Minorista</td>
+                <td>SO@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php" ><img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>10</td> 
+                <td>SU</td>
+                <td>Minorista</td>
+                <td>SU@gmail.com</td>
+                <td>123-456-7890</td>
+                <td>                
+                    <a href="inProvedores.php"> <img src="../img/Imag/info.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPE()"><img src="../img/Imag/No.png" alt=""></a>
+                    <a href="#" onclick="abrirVentanaPA()"><img src="../img/Imag/edit.png" alt=""></a>
+                    
+                </td>
+            </tr>
+            
+        </table>
+        </article>
+    </div>
+    </div>
+
+    <div id="ventana-emergentePR">
+        <form class="form-usuarios">
+            <fieldset id="contenido-ventana">
+                <div class="header-registrar-usuario">
+                    <h2>Registrar Proveedor</h2>
+                    <a href="#" onclick="cerrarVentanaPR()">Cerrar</a>
+                </div>
+                <br>
+                <input type="text" name="nombre" id="nombre" class="inp-registrar-usuarios" placeholder="Nombre local" require> <br>
+                <input type="email" name="correo" id="correo"  class="inp-registrar-usuarios" placeholder="Correo" require> <br>
+                <input type="number" name="celular" id="celular" class="inp-registrar-usuarios" placeholder="Celular" require> <br>
+                <input type="text" name="contraseña" id="direccion" class="inp-registrar-usuarios" placeholder="Direccion" require> <br>
+                
+                
+                <button type="button"  class="bton registrarUsuario" onclick="Validacion()">Registrar</button>
+            </fieldset>
+          </form>        
+    </div>
+    <div id="ventana-emergentePA">
+        <form class="form-usuarios">
+            <fieldset id="contenido-ventana">
+                <div class="header-registrar-usuario">
+                    <h2>Actualizar Proveedor</h2>
+                    <a href="#" onclick="cerrarVentanaPA()">Cerrar</a>
+
+                    
+                </div>
+                <br>
+                <input type="text" name="nombre" class="inp-registrar-usuarios" placeholder="Nombre local"> <br>
+                <input type="email" name="correo" class="inp-registrar-usuarios" placeholder="Correo"> <br>
+                <input type="text" name="celular" class="inp-registrar-usuarios" placeholder="Celular"> <br>
+                <input type="text" name="contraseña" class="inp-registrar-usuarios" placeholder="Direccion"> <br>
+                
+                
+                <button type="button" onclick="validarAC()" class="bton registrarUsuario">Actualizar</button>
+            </fieldset>
+          </form>        
+    </div>
+    <div id="ventana-emergentePE">
+        <form class="form-usuarios">
+            <fieldset id="contenido-ventana">
+                <div class="header-registrar-usuario">
+                    <h2>Eliminar Proveedor</h2>
+                    <a href="#" onclick="cerrarVentanaPE()">Cerrar</a>
+
+                    
+                </div>
+                <br>
+                <h3>¿Desea Deshabilitar este proveedor?</h3>
+                
+                
+                <button class="bton registrarUsuario">Eliminar</button>
+            </fieldset>
+          </form>        
+    </div>
+    <div id="validacion-Buena-Prove">
+        <img class="validacion-img"  src="../img/correctamente-validacion.png" alt="">
+        <h3>¡Registro completo !</h3>
+        <p>Se registró al Proveedor <br> correctamente</p>
+    </div>
+    <div id="validacion-Mala-Prove">
+        <img class="validacion-img"  src="../img/erronea-validacion.png" alt="">
+        <h3>Error!</h3>
+        <p>Verifique que los campos se <br> completen correctamente </p>
+    </div>
+    <div id="validacion-actualizar-Prove">
+        <img class="validacion-img"  src="../img/correctamente-validacion.png" alt="">
+        <h3>¡Datos Actualizados!</h3>
+        <p>Los datos se actualizaron correctamente</p>
+    </div>
+    
+    <script src="../../controlador/proveedores.js"></script>
+
+    <?php
+        include '../footer.php';
+    ?>
+    
